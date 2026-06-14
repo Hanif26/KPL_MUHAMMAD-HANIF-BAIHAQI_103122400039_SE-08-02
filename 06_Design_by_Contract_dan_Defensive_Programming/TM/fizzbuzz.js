694a648d7a8fe7d82086e3d34de3bc50fe529de8
Hanif26 <hanifbhq26@gmail.com>
2026-06-14 23:05:24 +0700

    Mengunggah seluruh sisa folder modul dan tugas besar

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TM/fizzbuzz.js
@@ -0,0 +1,26 @@
+function is_not_fizzbuzz(number) {
+  if (typeof number !== 'number' ||
+      !Number.isFinite(number) ||
+      !Number.isInteger(number)) {
+    throw new TypeError('Input harus bilangan bulat yang valid');
+  }
+
+  return !(number % 3 === 0 || number % 5 === 0);
+}
+
+console.log(is_not_fizzbuzz(1))        // true
+console.log(is_not_fizzbuzz(3))        // false
+console.log(is_not_fizzbuzz(5))        // false
+console.log(is_not_fizzbuzz(30))       // false
+console.log(is_not_fizzbuzz(7))        // true
+
+// Membungkus pengujian error agar tidak menghentikan program
+const testInputs = [null, NaN, Infinity];
+
+testInputs.forEach(input => {
+    try {
+        console.log(is_not_fizzbuzz(input));
+    } catch (error) {
+        console.log(`Tertangkap Error: ${error.message}`);
+    }
+});
